0db73947d162912e083fd479e3b60c3d505c4c63
finny <katthoriq@gmail.com>
2025-02-27 17:36:54 +0700

    Adjusti mobile views

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
   integrations: [tailwind()],
+  vite: {
+    plugins: [basicSsl()]
+  }
 });
