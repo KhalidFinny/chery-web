f09f4b4fa78e2330d77dbbf6bf99b5e819b2a1ec
Rakai Seto <79091133+RakaiSeto@users.noreply.github.com>
2025-11-14 20:29:27 +0700

    Remove Node adapter and set output to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import node from "@astrojs/node";
 
 export default defineConfig({
+  output: "static",
   integrations: [tailwind()],
-  adapter: node({
-    mode: "middleware",
-  }),
   server: {
     host: true
   }
